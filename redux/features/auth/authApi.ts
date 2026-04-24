605cf1bc64273784a22d40aa4ade6f05b48be4dc
tahfeezmizan <mizanahmed5562@gmail.com>
2026-04-25 04:34:57 +0600

    feat: implement authentication flow with login, forgot password, and OTP verification forms using RTK Query

--- redux/features/auth/authApi.ts
+++ redux/features/auth/authApi.ts
@@ -1,18 +1,7 @@
-import { removeUser } from "../../slice/userSlice";
 import { baseApi } from "../baseApi";
 
 const authApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    // create user
-    signUpUser: builder.mutation({
-      query: (data) => ({
-        url: `/auth/signup`,
-        method: "POST",
-        body: data,
-      }),
-      invalidatesTags: ["Auth"],
-    }),
-
     // login user
     loginUser: builder.mutation({
       query: (data) => ({
@@ -23,28 +12,8 @@ const authApi = baseApi.injectEndpoints({
       invalidatesTags: ["Auth"],
     }),
 
-    // verify user
-    verifyUser: builder.mutation({
-      query: (data) => ({
-        url: "/auth/verify-account",
-        method: "POST",
-        body: data,
-      }),
-      invalidatesTags: ["Auth"],
-    }),
-
-    // verify user
-    resendOTP: builder.mutation({
-      query: (data) => ({
-        url: "/auth/resend-otp",
-        method: "POST",
-        body: data,
-      }),
-      invalidatesTags: ["Auth"],
-    }),
-
-    // verify user
-    forgetPasswordSendOTP: builder.mutation({
+    // forgot password
+    forgetPassword: builder.mutation({
       query: (data) => ({
         url: "/auth/forget-password",
         method: "POST",
@@ -53,54 +22,32 @@ const authApi = baseApi.injectEndpoints({
       invalidatesTags: ["Auth"],
     }),
 
-    // ✅ Reset password (with token in headers)
-    setNewPassword: builder.mutation({
-      query: ({ token, body }) => ({
-        url: `/auth/reset-password?token=${token}`,
+    // verify otp
+    verifyOtp: builder.mutation({
+      query: (data) => ({
+        url: "/auth/verify-account",
         method: "POST",
-        body,
+        body: data,
       }),
       invalidatesTags: ["Auth"],
     }),
 
-  changePassword: builder.mutation({
-    query: (data) => ({
-      url: "/auth/change-password",
-      method: "POST",
-      body: data,
-    }),
-    invalidatesTags: ["Auth"],
-  }),
-
-    // logout user
-    logoutUser: builder.mutation({
-      query: () => ({
-        url: "/auth/logout",
+    // resend otp
+    resendOtp: builder.mutation({
+      query: (data) => ({
+        url: "/auth/verify-account",
         method: "POST",
+        body: data,
       }),
       invalidatesTags: ["Auth"],
-      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
-        try {
-          await queryFulfilled;
-          // Clear RTK Query cache
-          dispatch(baseApi.util.resetApiState());
-          // Clear Redux user state
-          dispatch(removeUser()); // This will clear your user slice
-        } catch (error) {
-          console.error("Logout failed:", error);
-        }
-      },
     }),
   }),
 });
 
 export const {
-  useSignUpUserMutation,
-  useVerifyUserMutation,
-  useForgetPasswordSendOTPMutation,
-  useResendOTPMutation,
   useLoginUserMutation,
-  useLogoutUserMutation,
-  useSetNewPasswordMutation,
-  useChangePasswordMutation
+  useForgetPasswordMutation,
+  useVerifyOtpMutation,
+  useResendOtpMutation,
 } = authApi;
+
